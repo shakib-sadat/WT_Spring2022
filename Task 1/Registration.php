2dd5bae56cce7d32d8e1bd7eacaa64ad00871788
Shakib Sadat Shanto <62327880+shakib-sadat@users.noreply.github.com>
2022-02-02 09:37:43 +0600

    Add files via upload

---
+++ Task 1/Registration.php
@@ -0,0 +1,55 @@
+<!Doctype HTML>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h3>Registration Form</h3>
+        <hr>
+        <table>
+        <tr>
+        <td>First Name:</td>
+        <td><input type="text" name="name"> <br> </td>
+        </tr>
+        <tr>
+        <td>Last Name:</td> 
+        <td><input type="text" name="name"> <br> </td>
+        </tr>
+        <tr>
+        <td>Age:</td> 
+        <td><input type="number"> <br></td>
+        </tr>
+        <tr>
+        <td>Designation: </td>
+        <td>
+        <input type=radio name="designation"> Junior Programmer 
+        <input type=radio name="designation"> Senior Programmer 
+        <input type=radio name="designation"> Project Head <br> 
+        </td>
+        </tr>
+        <tr>
+        <td>Preferred Language: </td>
+        <td>
+        <input type="checkbox"> JAVA
+        <input type="checkbox"> PHP 
+        <input type="checkbox"> C++ <br>
+        </td>
+        </tr>
+        <tr>
+        <td>Email:</td> 
+        <td><input type="text" name=email> <br></td>
+        </tr>
+        <tr>
+        <td>Password:</td> <td><input type="password"> <br></td>
+        </tr>
+        <tr>
+        <td>Please choose a file:</td> <td><input type="file"> <br></td>
+        </tr>
+        <tr>
+        <td><input type = "submit">
+        <input type = "reset"></td>
+        </tr>
+        </table>
+
+    </body>
+</html>
