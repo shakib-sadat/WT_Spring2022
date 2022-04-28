a7c4fcc1db64c3137e98bc1894fc8f502d6f738e
Shakib Sadat Shanto <62327880+shakib-sadat@users.noreply.github.com>
2022-04-28 20:54:37 +0600

    Add files via upload

---
+++ Task5/js/validate.js
@@ -0,0 +1,121 @@
+document.getElementById("fname").addEventListener("keyup", function() {
+    if (document.getElementById("fname").value == "") {
+        document.getElementById("errorfname").innerHTML =
+            "Please fill out First Name";
+    } else if (
+        /^[a-zA-Z]+$/.test(document.getElementById("fname").value) == false
+    ) {
+        document.getElementById("errorfname").innerHTML = "Inavalid First Name";
+    } else {
+        document.getElementById("errorfname").innerHTML = "";
+    }
+});
+
+document.getElementById("lname").addEventListener("keyup", function() {
+    if (document.getElementById("lname").value == "") {
+        document.getElementById("errorlname").innerHTML =
+            "Please fill out Last Name";
+    } else if (
+        /^[a-zA-Z]+$/.test(document.getElementById("lname").value) == false
+    ) {
+        document.getElementById("errorlname").innerHTML = "Inavalid Last Name";
+    } else {
+        document.getElementById("errorlname").innerHTML = "";
+    }
+});
+
+document.getElementById("uname").addEventListener("keyup", function() {
+    if (document.getElementById("uname").value == "") {
+        document.getElementById("erroruname").innerHTML =
+            "Please fill out Username";
+    } else if (
+        /^[a-zA-Z]+[0-9]*$/.test(document.getElementById("uname").value) == false
+    ) {
+        document.getElementById("erroruname").innerHTML = "Inavalid Username";
+    } else {
+        document.getElementById("erroruname").innerHTML = "";
+    }
+});
+
+document.getElementById("age").addEventListener("keyup", function() {
+    if (document.getElementById("age").value == "") {
+        document.getElementById("errorage").innerHTML = "Please fill out Age";
+    } else if (/^[0-9]+$/.test(document.getElementById("age").value) == false) {
+        document.getElementById("errorage").innerHTML = "Inavalid Age";
+    } else if (
+        document.getElementById("age").value < 18 ||
+        document.getElementById("age").value > 70
+    ) {
+        document.getElementById("errorage").innerHTML =
+            "Age Must be between 18 to 70";
+    } else {
+        document.getElementById("errorage").innerHTML = "";
+    }
+});
+
+document.getElementById("email").addEventListener("keyup", function() {
+    if (document.getElementById("email").value == "") {
+        document.getElementById("erroremail").innerHTML = "Please fill out Email";
+    } else if (
+        /^\S+@\S+[.]\S+$/.test(document.getElementById("email").value) == false
+    ) {
+        document.getElementById("erroremail").innerHTML = "Inavalid Email Format";
+        console.log("alalalalal");
+    } else {
+        document.getElementById("erroremail").innerHTML = "";
+    }
+});
+
+document.getElementById("password").addEventListener("keyup", function() {
+    if (document.getElementById("password").value == "") {
+        document.getElementById("errorpassword").innerHTML =
+            "Please fill out Password";
+    } else if (
+        /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*().]).{8,}$/.test(
+            document.getElementById("password").value
+        ) == false
+    ) {
+        document.getElementById("errorpassword").innerHTML =
+            "Inavalid Password Format";
+    } else {
+        document.getElementById("errorpassword").innerHTML = "";
+    }
+});
+
+document.getElementById("nid").addEventListener("keyup", function() {
+    if (document.getElementById("nid").value == "") {
+        document.getElementById("errornid").innerHTML = "Please fill out Nid";
+    } else if (
+        /^[+0-9]{13}$/.test(document.getElementById("nid").value) == false
+    ) {
+        document.getElementById("errornid").innerHTML = "Inavalid Nid Format";
+    } else {
+        document.getElementById("errornid").innerHTML = "";
+    }
+});
+document.getElementById("contact").addEventListener("keyup", function() {
+    if (document.getElementById("contact").value == "") {
+        document.getElementById("errorcontact").innerHTML =
+            "Please fill out contact";
+    } else if (
+        /^[+0-9]{11}$/.test(document.getElementById("contact").value) == false
+    ) {
+        document.getElementById("errorcontact").innerHTML = "Inavalid contact";
+    } else {
+        document.getElementById("errorcontact").innerHTML = "";
+    }
+});
+
+document.getElementById("language").addEventListener("keyup", function() {
+    if (document.getElementById("language").value == "") {
+        document.getElementById("errorlanguage").innerHTML =
+            "Please fill out Language";
+    } else if (
+        /^[a-zA-Z]+$/.test(document.getElementById("language").value) == false
+    ) {
+        document.getElementById("errorlanguage").innerHTML =
+            "Inavalid Language Format";
+    } else {
+        document.getElementById("errorlanguage").innerHTML = "";
+    }
+});
---
+++ Task5/view/registration.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+  <head><link rel="stylesheet"  href="../css/teststyle.css"> </head>
+  <body>
+    <p class="form">Registration Form</p>
+    <hr>
+  <div class="registration">
+    <form action=''   method='post'>   
+    <table>
+        <td>User Type:</td>
+        <td><input type="radio" name="usertype" value="admin" /> Admin 
+        <input type="radio" name="usertype" value="employee" /> Employee
+        <input type="radio" name="usertype" value="developer" /> Developer
+      </td>
+      </tr>
+      <tr>
+        <td><br>User Name:</td>
+        <td><input type="text" name="uname" id="uname" />  <p id = "erroruname" > </p></td>
+
+      </tr>
+      <tr>
+        <td>First Name:</td>
+        <td><input type="text" name="fname" id="fname" /> <p id = "errorfname" > </p></td>
+      </tr>
+ 
+      <tr>
+        <td>Last Name:</td>
+        <td><input type="text" name="lname" id="lname" /> <br /> <p id = "errorlname" > </p></td>
+ 
+      </tr>
+      <tr>
+        <td>Age:</td>
+        <td><input type="number" name="age" id="age" /> <br /> <p id = "errorage" ></td>
+      </tr>
+
+      <tr>
+        <td>Data of Birth:</td>
+        <td><input type="date" name="dob" id="dob" /> <br /> <p id="errordob"> </p> </td>
+      </tr>
+      
+      <tr>
+      <td>Email:</td>
+        <td><input type="text" name="email" id="email" /> <p id = "erroremail" > </p> <br /></td>
+        
+      </tr>
+      <tr>
+        <td>Password:</td>
+        <td><input type="password" name="password" id="password" />  <p id = "errorpassword" > </p> </td>
+      </tr>
+      <tr>
+      <td>Gender:</td>
+      <td><input type="radio" name="gender" value="male" /> Male 
+      <input type="radio" name="gender" value="female" /> female
+      </td>
+      </tr>
+      <tr>
+      <td>Nid No:</td>
+      <td><input type="number" name="nid" id="nid"> <br> <p id = "errornid" > </p></td>  
+      </tr>
+      <tr>
+      <td>Contact No:</td>
+      <td><input type="number" name="contact" id="contact"> <br> <p id = "errorcontact" > </p> </td>
+      </tr>
+      <tr>
+      <td>Address:</td>
+      <td><input type="text" id="address" name="address"></td>
+      </tr>
+      <tr>
+        <td>Preferred Language:</td>
+        <td>
+          <input type="text" name="language"  id="language" /> <br>
+          <p id = "errorlanguage" > </p>
+        </td>
+      </tr>
+      <tr>
+        <td>Designation:</td>
+        <td>
+          <input type="radio" name="designation" value="Junior Programmer" /> Junior Programmer
+          <input type="radio" name="designation" value="Senior Programmers" /> Senior Programmer
+          <input type="radio" name="designation" value="Prjocet Head" /> Project Head <br />
+        </td>
+      </tr>
+      <tr>
+      <td>Experience:</td>
+      <td><input type="text" id="experience" name="experience"></td>
+      </tr>
+        <td><input type="submit" name="submit" id="submitbutton" /> <input type="reset" name="reset" id="resetbutton" /></td>
+      </tr>
+    </table>
+
+    </form>
+    </div>
+        <script src="../js/validate.js"> </script>
+      
+  </body>
+</html>
